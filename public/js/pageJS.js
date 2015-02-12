84a2ca29d83286b1217897be8d074d1089ac4478
Nikos <npgeorgiou@yahoo.gr>
2015-02-12 15:26:10 +0100

    readme

--- public/js/pageJS.js
+++ public/js/pageJS.js
@@ -20,12 +20,13 @@ $(function() {
 
         //If window is small enough, enable sidebar push menu
         if ($(window).width() <= 992) {
+
             $('.row-offcanvas').toggleClass('active');
             $('.left-side').removeClass("collapse-left");
             $(".right-side").removeClass("strech");
             $('.row-offcanvas').toggleClass("relative");
         } else {
-            //Else, enable content stretching
+            //Else, enable content streching
             $('.left-side').toggleClass("collapse-left");
             $(".right-side").toggleClass("strech");
         }
